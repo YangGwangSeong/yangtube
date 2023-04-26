1f8a136787e381f20d2f39dfff0ec1241e618912
YangGwangSeong <rhkdtjd_12@naver.com>
2023-04-26 07:40:03 -0700

    feat: JwtStrategy 설정

---
+++ backend/src/auth/auth.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { getJWTConfig } from 'src/config/jwt.config';
+import { JwtStrategy } from './strategies/jwt.strategy';
+
+@Module({
+	controllers: [AuthController],
+	providers: [AuthService, JwtStrategy],
+	imports: [
+		ConfigModule,
+		JwtModule.registerAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: getJWTConfig,
+		}),
+	],
+})
+export class AuthModule {}
---
+++ backend/src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+	constructor(private readonly configService: ConfigService) {
+		super({
+			jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+			ignoreExpiration: true,
+			secretOrKey: configService.get('JWT_SECRET'),
+		});
+	}
+
+	async validate(payload: { userId: string }) {
+		// const user = await this.usersService.findOne(payload.userId);
+		// if (!user) {
+		// throw new UnauthorizedException();
+		// }
+		// return user;
+	}
+}
