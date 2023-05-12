83851c0f1feef8d4036d033f15d5a713dd8576a0
YangGwangSeong <rhkdtjd_12@naver.com>
2023-05-11 23:50:53 -0700

    feat: 버튼 UI 구성 요소

--- client/components/ui/button/Button.tsx
+++ client/components/ui/button/Button.tsx
@@ -1,7 +1,18 @@
-import React, { FC } from 'react';
+import React, { FC, PropsWithChildren } from 'react';
+import cn from 'classnames';
+import styles from './Button.module.scss';
+import { Button } from './button.interface';
 
-const Button: FC = () => {
-	return <div>Button</div>;
+const Button: FC<PropsWithChildren<Button>> = ({
+	children,
+	className,
+	...rest
+}) => {
+	return (
+		<button className={cn(styles.button, className)} {...rest}>
+			{children}
+		</button>
+	);
 };
 
 export default Button;
