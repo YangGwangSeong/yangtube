0c69426b669f6dd7f5035a74fda980758a554611
YangGwangSeong <rhkdtjd_12@naver.com>
2023-05-15 03:03:38 -0700

    feat: axios interceptor 작성

--- client/README.md
+++ client/README.md
@@ -7,4 +7,6 @@ $ tailwind css
 $ npm i axios react-query react-hook-form classnames
 $ npm i sass
 $ npm i react-icons
+$ npm i js-cookie
+$ npm i --save-dev @types/js-cookie
 ```
---
+++ client/api/interceptor.tsx
@@ -0,0 +1,28 @@
+import axios, { AxiosHeaders, AxiosRequestConfig } from 'axios';
+import Cookies from 'js-cookie';
+
+export const getContentType = () => ({
+	'Content-Type': 'application/json',
+});
+
+export const axiosClassic = axios.create({
+	baseURL: process.env.API_URL,
+	headers: getContentType(),
+});
+
+export const instance = axios.create({
+	baseURL: process.env.API_URL,
+	headers: getContentType(),
+});
+
+instance.interceptors.request.use(config => {
+	const accessToken = Cookies.get('accessToken');
+
+	if (config.headers && accessToken) {
+		config.headers.Authorization = `Bearer ${accessToken}`;
+	}
+
+	return config;
+});
+
+export default instance;
