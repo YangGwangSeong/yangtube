10a386d2a75fd384a8b4d7e226d9408ec292679d
YangGwangSeong <rhkdtjd_12@naver.com>
2023-05-22 01:50:49 -0700

    feat: update-profile, get-profile 추가

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,7 +1,25 @@
-import { Controller } from '@nestjs/common';
+import {
+	Body,
+	Controller,
+	HttpCode,
+	Put,
+	UsePipes,
+	ValidationPipe,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
+import { UserDto } from './dto/user.dto';
 
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+	constructor(private readonly usersService: UsersService) {}
+
+	@UsePipes(new ValidationPipe())
+	@HttpCode(200)
+	@Put('profile')
+	async profile(@Body() dto: UserDto) {
+		return this.usersService.updateProfile(
+			'441c8f2c-5ac9-4e93-a245-ef5d8f8e0a7f',
+			dto,
+		);
+	}
 }
--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,4 +1,52 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { UserDto } from './dto/user.dto';
+import { genSalt, hash } from 'bcryptjs';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+	constructor(private readonly prisma: PrismaService) {}
+
+	async byId(_id: string) {
+		const user = await this.prisma.user.findUnique({
+			where: {
+				id: _id,
+			},
+		});
+
+		if (!user) throw new UnauthorizedException('User not found');
+
+		return user;
+	}
+
+	async updateProfile(_id: string, dto: UserDto): Promise<void> {
+		const user = await this.byId(_id);
+		const isSameUser = await this.prisma.user.findUnique({
+			where: {
+				email: dto.email,
+			},
+		});
+
+		if (isSameUser && _id !== isSameUser.id)
+			throw new UnauthorizedException('Email is busy');
+
+		if (dto.password) {
+			const salt = await genSalt(10);
+			user.password = await hash(dto.password, salt);
+		}
+
+		user.email = dto.email;
+		user.name = dto.name;
+		user.description = dto.description;
+		user.location = dto.location;
+		user.bannerPath = dto.bannerPath;
+		user.avatarPath = dto.avatarPath;
+
+		const updateUser = await this.prisma.user.update({
+			where: {
+				id: _id,
+			},
+			data: user,
+		});
+	}
+}
