65953e90aafe54c7bbc66a8496189eb208a42b68
YangGwangSeong <rhkdtjd_12@naver.com>
2023-06-04 09:01:29 -0800

    feat: uploadMediaFile api 생성

--- backend/README.md
+++ backend/README.md
@@ -11,4 +11,7 @@ $ npm install prisma --save-dev
 $ npm install @prisma/client
 $ npm install class-transformer
 $ npm install --save @nestjs/serve-static       // SPA 정적 서비스를 지원하기 위한 모듈
+$ npm i -D @types/multer                        // 파일 업로드
+$ npm i fs-extra                                // 파일 업로드
+$ npm i -D @types/fs-extra                      // 파일 업로드
 ```
--- backend/src/media/media.service.ts
+++ backend/src/media/media.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { MediaResponse } from './media.interface';
+import { ensureDir, writeFile } from 'fs-extra';
+import { path } from 'app-root-path';
 
 @Injectable()
-export class MediaService {}
+export class MediaService {
+	async saveMedia(
+		mediaFile: Express.Multer.File,
+		folder = 'default',
+	): Promise<MediaResponse> {
+		const uploadFolder = `${path}/uploads/${folder}`;
+		await ensureDir(uploadFolder);
+
+		await writeFile(
+			`${uploadFolder}/${mediaFile.originalname}`,
+			mediaFile.buffer,
+		);
+
+		return {
+			url: `/uploads/${folder}/${mediaFile.originalname}`,
+			name: mediaFile.originalname,
+		};
+	}
+}
