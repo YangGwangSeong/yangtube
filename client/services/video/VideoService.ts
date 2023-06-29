c61250fa426c9734f115cd852e39d732ca5e7362
YangGwangSeong <rhkdtjd_12@naver.com>
2023-06-28 23:54:10 -0700

    feat: video api를 호출하는 video-service 작성

--- client/services/video/VideoService.ts
+++ client/services/video/VideoService.ts
@@ -1,1 +1,20 @@
-export const UserService = {};
+import { ResponseVideo } from '@/shared/interfaces/video.interface';
+import axios, { axiosClassic } from '../../api/interceptor';
+
+export const VideoService = {
+	async getVideoByUser(userId: string) {
+		return axios.get<ResponseVideo[]>(`/video/by-user/${userId}`);
+	},
+
+	async getVideoByCurrentUser() {
+		return axios.get<ResponseVideo[]>(`/video/by-user-private`);
+	},
+
+	async getVideoById(id: string) {
+		return axiosClassic.get<ResponseVideo>(`/video/${id}`);
+	},
+
+	async getAllVideos(id: string) {
+		return axiosClassic.get<ResponseVideo[]>(`/video`);
+	},
+};
