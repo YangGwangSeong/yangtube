440c9d7fc06156ac7dc0dff284353948d4c321c9
YangGwangSeong <rhkdtjd_12@naver.com>
2023-06-29 00:22:31 -0700

    fix: video-service 작성 완료

--- client/services/video/VideoService.ts
+++ client/services/video/VideoService.ts
@@ -1,20 +1,48 @@
-import { ResponseVideo } from '@/shared/interfaces/video.interface';
+import { ResponseVideo, VideoDto } from '@/shared/interfaces/video.interface';
 import axios, { axiosClassic } from '../../api/interceptor';
 
 export const VideoService = {
-	async getVideoByUser(userId: string) {
-		return axios.get<ResponseVideo[]>(`/video/by-user/${userId}`);
+	async getVideosByUser(userId: string) {
+		return axios.get<ResponseVideo[]>(`/videos/by-user/${userId}`);
 	},
 
-	async getVideoByCurrentUser() {
-		return axios.get<ResponseVideo[]>(`/video/by-user-private`);
+	async getVideosByCurrentUser() {
+		return axios.get<ResponseVideo[]>(`/videos/by-user-private`);
 	},
 
 	async getVideoById(id: string) {
-		return axiosClassic.get<ResponseVideo>(`/video/${id}`);
+		return axiosClassic.get<ResponseVideo>(`/videos/${id}`);
 	},
 
 	async getAllVideos(id: string) {
-		return axiosClassic.get<ResponseVideo[]>(`/video`);
+		return axiosClassic.get<ResponseVideo[]>(`/videos`);
+	},
+
+	async getMostPopular(id: string) {
+		return axiosClassic.get<ResponseVideo[]>(`/videos/most-popular`);
+	},
+
+	async getVideoPrivateById(id: string) {
+		return axios.get<ResponseVideo>(`/videos/get-private/${id}`);
+	},
+
+	async createVideo() {
+		return axios.post<string>(`/videos`);
+	},
+
+	async updateVideo(id: string, body: VideoDto) {
+		return axios.put<string>(`/videos/${id}`, body);
+	},
+
+	async updateViews(id: string) {
+		return axios.put<string>(`/videos/update-views/${id}`);
+	},
+
+	async updateLikes(id: string) {
+		return axios.put<string>(`/videos/update-likes/${id}`);
+	},
+
+	async deleteVideo(id: string) {
+		return axios.delete<void>(`/videos/${id}`);
 	},
 };
