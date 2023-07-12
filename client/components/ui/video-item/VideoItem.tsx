baf3c6d200e1ade5ca83d132a362c6965063fcc1
YangGwangSeong <rhkdtjd_12@naver.com>
2023-07-11 23:57:20 -0700

    feat: create video-duration

--- client/components/ui/video-item/VideoItem.tsx
+++ client/components/ui/video-item/VideoItem.tsx
@@ -1,10 +1,10 @@
 import { ResponseVideo } from '@/shared/interfaces/video.interface';
 import { formatNumberToK } from '@/utils/formatNumberToK';
 import Image from 'next/image';
-import React, { FC, useRef } from 'react';
+import React, { FC } from 'react';
+import VideoDuration from './VideoDuration';
 
 const VideoItem: FC<{ item: ResponseVideo }> = ({ item }) => {
-	const ref = useRef<HTMLVideoElement>(null);
 	return (
 		<div className="video_item">
 			<div className="thumbnail">
@@ -14,15 +14,7 @@ const VideoItem: FC<{ item: ResponseVideo }> = ({ item }) => {
 					width={163}
 					height={91}
 				></Image>
-				<video className="hidden" ref={ref}>
-					<source type="video/mp4"></source>
-				</video>
-				<time>
-					{ref.current?.duration &&
-						Math.floor(ref.current?.duration / 60) +
-							':' +
-							('0' + Math.floor(ref.current.duration % 60)).slice(-2)}
-				</time>
+				<VideoDuration videoPath={item.videoPath}></VideoDuration>
 			</div>
 			<div className="author">{item.user?.name}</div>
 			<div className="name">{item.name}</div>
